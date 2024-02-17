2785fbaaab67ba885f8975dd54ed30801338081b
apinto @ lardafada <senhoralexandrepinto@protonmail.com>
2024-02-17 13:14:00 +0000

    further improvements in tiddler creation

--- tiddler.scm
+++ tiddler.scm
@@ -1,8 +1,11 @@
 (include "utils.scm")
+(include "api-consts.scm")
 (include "mylogger.scm")
 (import (only (chicken file)
 			  file-exists?
 			  delete-file*)
+		(only (chicken string)
+			  string-translate)
 		(only (chicken io)
 			  write-line)
 		(only (chicken time posix)
@@ -10,12 +13,26 @@
 			  seconds->utc-time)
 		(only (srfi-1)
 			  any
-			  find))
-(import (chicken condition))
+			  find)
+		(only (srfi-13)
+			  string-downcase
+			  ))
 
 
 ; tiddler-content, timestamp, filepath are String
-; logfile is EXISTING-RECORDS
+; logfile is String, sourced from the EXISTING-RECORDS defined in api-consts.scm,
+; and it keeps track of archived `groupid`
+
+; creates a filename from record-info
+; record-info -> String
+(define (get-album-art-filename record-info)
+   (define norm (lambda (str) (string-translate (string-downcase str) " " "-")))
+   (norm (string-append (record-info-title record-info) "-" (record-info-artist record-info))))
+
+;TODO 
+;(string-append TIDDLERS-PATH "/"
+			   ;(get-album-art-filename record-info)
+			   ;"." extension)
 
 ; creates a tiddler-content String from record-info
 ; record-info timestamp -> tiddler-content
@@ -30,6 +47,11 @@
 				  "year: " (number->string (record-info-year record-info)) "\n"
 				  "type: text/vnd.tiddlywiki"))
 
+; creates a image-meta-tiddler String from record-info
+; record-info -> tiddler-content
+(define (create-image-meta-tiddler record-info)
+   (string-append "title: " (get-album-art-filename record-info) "\n"
+				  "type: image/" (get-extension-from-image-uri (record-info-image-uri record-info)) "\n"))
 
 ; creates a file from a tiddler-content
 ; tiddler-content filepath -> Boolean
@@ -39,8 +61,25 @@
 					 (let ((exn-message ((condition-property-accessor 'exn 'message) exn)))
 						 (log-message 5 exn-message))
 					 #f)
-				   (with-output-to-file filepath (lambda () tiddler-content))
+				   (with-output-to-file filepath (lambda () (write-string tiddler-content)))
 				   #t))
+
+; routine to save an album-art file; it does:
+; - send a request to the image endpoint;
+; - save the image;
+; - create the meta tiddler.
+; record-info -> boolean
+(define (produce-record-info-album-art record-info tiddlers-path)
+  (let*-values (((image-uri) (record-info-image-uri record-info))
+				((album-art-filename) (get-album-art-filename record-info))
+				; NOTE check for '/' in tiddlers-path
+				((album-art-filepath) (string-append tiddlers-path album-art-filename))
+				((meta-tiddler-filepath) (string-append album-art-filepath ".meta"))
+				((image-saving-bool uri-obj image-response)
+				 (request-and-save-image-file image-uri album-art-filepath)))
+    (if image-saving-bool
+		(create-tiddler-file (create-image-meta-tiddler record-info) meta-tiddler-filepath)
+		#f)))
 
 ; append a new groupid to the logfile
 ; record-info -> 
